343543f045a0af4dca7c5062d4c8531c934ffb3c
Ahmad <ahmadbh.mail@gmail.com>
2023-05-17 00:15:16 +0430

    add ShopingSite/Models/Product.cs

---
+++ ShopingSite/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingSite.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            Categories = new List<Category>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  int Description { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+    }
+}
