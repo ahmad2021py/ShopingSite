60bf91eda8ac9ea0161b27b264c4d3558f3304ab
Ahmad <ahmadbh.mail@gmail.com>
2023-05-17 00:18:50 +0430

    add ShopingSite/Models/Item.cs

---
+++ ShopingSite/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingSite.Models
+{
+    public class Item
+    {
+
+
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+
+
+
+    }
+}
