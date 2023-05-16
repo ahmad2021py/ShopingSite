9ebd633564c67d2a188a70d12d6757b7edfe341d
Ahmad <ahmadbh.mail@gmail.com>
2023-05-17 00:39:44 +0430

    add ShopingSite/Models/Cart.cs model

---
+++ ShopingSite/Models/Cart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingSite.Models
+{
+    public class Cart
+    {
+
+        public Cart()
+        {
+            CartItems = new List<CartItem>();
+        }
+        public int OrderId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
+        public void AddItem(CartItem item)
+        {
+            if (CartItems.Exists(n => n.Id == item.Id))
+            {
+                CartItems.Find(n => n.Id == item.Id).Quantity += 1;
+            }
+
+            else
+            {
+
+                CartItems.Add(item);
+            }
+        }
+        public void Remove(int itemId)
+        {
+            var item = CartItems.SingleOrDefault(n => n.Item.Id == itemId);
+            if (item!=null&& item.Quantity <=1 )
+            {
+                CartItems.Remove(item);
+            }
+            else if (item != null) 
+            {
+                item.Quantity -= 1;
+            }
+
+
+        }
+
+    }
+}
