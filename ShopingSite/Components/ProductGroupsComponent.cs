a20352eb8f1516e9d8ac538d0910ae62e91c2cfb
Ahmad <ahmadbh.mail@gmail.com>
2023-05-18 00:37:55 +0430

    add ShopingSite/Components/ProductGroupsComponent.cs -------------- add ShopingSite/Views/Components/ProductGroupsComponent.cshtml ---------------- Update ShopingSite/Views/Shared/_Layout.cshtml

---
+++ ShopingSite/Components/ProductGroupsComponent.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopingSite.Data;
+
+namespace ShopingSite.Components
+{
+    public class ProductGroupsComponent : ViewComponent
+    {
+        private ShopingSiteContext _context;
+
+        public ProductGroupsComponent(ShopingSiteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/Components/ProductGroupsComponent.cshtml", _context.Categories);
+        }
+    }
+}
