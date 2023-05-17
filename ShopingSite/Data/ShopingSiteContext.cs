7ba7ba6cb9b34f19d3bd39405acb31c22d367328
Ahmad <ahmadbh.mail@gmail.com>
2023-05-17 18:11:33 +0430

    Add OnModelCreating Method  to ShopingSite/Data/ShopingSiteContext.cs For Seed Data To Categoty Table while Generate Database .

--- ShopingSite/Data/ShopingSiteContext.cs
+++ ShopingSite/Data/ShopingSiteContext.cs
@@ -17,6 +17,40 @@ namespace ShopingSite.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
+            #region Seed Data Category
+
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                Id = 1,
+                Name = "Asp.Net Core",
+                Description = "Asp.Net Core 3"
+            },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "لباس ورزشی",
+                    Description = "گروه لباس ورزشی"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "ساعت مچی",
+                    Description = "ساعت مچی"
+                },
+                new Category()
+                {
+                    Id = 4,
+                    Name = "لوازم منزل",
+                    Description = "لوازم منزل"
+                }
+            );
+
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
