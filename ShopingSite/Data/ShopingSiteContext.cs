eabbb578d3f5a68dbbd6efaa9500d910ca5e3503
Ahmad <ahmadbh.mail@gmail.com>
2023-05-17 17:32:56 +0430

    Add Context ShopingSite/Data/ShopingSiteContext.cs to project

---
+++ ShopingSite/Data/ShopingSiteContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopingSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingSite.Data
+{
+    public class ShopingSiteContext:DbContext // attachment 1
+    {
+        public ShopingSiteContext(DbContextOptions<ShopingSiteContext> options) : base(options)
+        {
+
+
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
