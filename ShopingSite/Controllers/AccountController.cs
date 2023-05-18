a290d62a9a66e2d0a4416fecefd4d755a3e87de1
Ahmad <ahmadbh.mail@gmail.com>
2023-05-18 17:46:58 +0430

    add User Register . add ShopingSite/Models/AccountViewModels.cs add ShopingSite/Controllers/AccountController.cs add ShopingSite/Views/Account/Register.cshtml Update ShopingSite/Views/Shared/_Layout.cshtml

---
+++ ShopingSite/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopingSite.Models;
+
+namespace ShopingSite.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        public IActionResult Register() // this method by default is HttpGet
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+            return View();
+        }
+    }
+}
---
+++ ShopingSite/Models/AccountViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopingSite.Models
+{
+    public class RegisterViewModel // tip : The class name is different from the class file name
+    {
+
+        [MaxLength(300)]
+        [EmailAddress]
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [Display(Name = "تکرار کلمه عبور")]
+        public string RePassword { get; set; }
+    }
+}
