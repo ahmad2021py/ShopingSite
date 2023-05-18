4ba97e647bf677bfe4e8d17a6b95c01fe199396a
Ahmad <ahmadbh.mail@gmail.com>
2023-05-18 17:21:36 +0430

    add migration : ShopingSite/Migrations/20230518124843_init UsersTbl.cs  For generate Users Table .

--- ShopingSite/Migrations/20230518124843_init UsersTbl.Designer.cs
+++ ShopingSite/Migrations/20230518124843_init UsersTbl.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopingSite.Data;
 
 namespace ShopingSite.Migrations
 {
     [DbContext(typeof(ShopingSiteContext))]
-    partial class ShopingSiteContextModelSnapshot : ModelSnapshot
+    [Migration("20230518124843_init UsersTbl")]
+    partial class initUsersTbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
